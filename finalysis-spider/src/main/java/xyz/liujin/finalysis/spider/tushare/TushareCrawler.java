508136a2d9fd589080db7a3c96c5a2079e1b884f
lbzhello <i191908577>
2021-01-07 00:19:33 +0800

    tushare daily 接口实现

---
+++ finalysis-spider/src/main/java/xyz/liujin/finalysis/spider/tushare/TushareCrawler.java
@@ -0,0 +1,56 @@
+package xyz.liujin.finalysis.spider.tushare;
+
+import cn.hutool.json.JSONUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
+import xyz.liujin.finalysis.spider.crawler.StockCrawler;
+import xyz.liujin.finalysis.spider.dto.KLineDto;
+import xyz.liujin.finalysis.spider.entity.Stock;
+import xyz.liujin.finalysis.spider.service.StockService;
+
+import java.io.IOException;
+
+public class TushareCrawler implements StockCrawler {
+    private static Logger logger = LoggerFactory.getLogger(TushareCrawler.class);
+
+    @Autowired
+    private StockService stockService;
+
+    public static void main(String[] args) {
+        TushareCrawler tushareCrawler = new TushareCrawler();
+        tushareCrawler.crawlKLine();
+    }
+
+    @Override
+    public Flux<Stock> crawlStock() {
+        return Flux.just();
+    }
+
+    @Override
+    public Flux<KLineDto> crawlKLine() {
+        // 爬取所有股票 K 线
+        TushareReq.daily.builder()
+//                .ts_code("")
+                .trade_date("20210104")
+                .build()
+                .req()
+                .subscribe(response -> {
+                    try {
+                        String body = response.body().string();
+                        TushareResp tushareResp = JSONUtil.toBean(body, TushareResp.class);
+                        System.out.println(tushareResp);
+                    } catch (IOException e) {
+                        logger.error("failed to call tushare.daily");
+                    }
+                });
+
+        return null;
+    }
+
+    @Override
+    public Flux<KLineDto> crawlKLine(String stockCode) {
+        return null;
+    }
+}
---
+++ finalysis-spider/src/main/java/xyz/liujin/finalysis/spider/tushare/TushareReq.java
@@ -0,0 +1,112 @@
+package xyz.liujin.finalysis.spider.tushare;
+
+import cn.hutool.json.JSONUtil;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import okhttp3.Response;
+import reactor.core.publisher.Flux;
+import xyz.liujin.finalysis.spider.util.HttpUtils;
+
+import java.util.Map;
+
+/**
+ * tushare http api 请求参数
+ * 官网：https://tushare.pro/
+ *
+ * 说明：
+ * N 表示非必填
+ * Y 表示必填
+ * 默认  Y 必填
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TushareReq {
+    /**
+     * tushare token；登录后从用户中心获取
+     */
+    public static final String TOKEN = "2ea612d41ee1fdb2fed04bb097debcbcee8d43eeba5570a2605d6914";
+
+    /**
+     * API 调用接口
+     */
+    public static final String API = "http://api.waditu.com";
+
+    /**
+     * 接口名称，比如stock_basic
+     */
+    private String api_name;
+    /**
+     * 用户唯一标识，可通过登录pro网站获取
+     */
+    private String token;
+    /**
+     * 接口参数，如daily接口中start_date和end_date
+     */
+    private Map<String, Object> params;
+    /**
+     * 字段列表，用于接口获取指定的字段，以逗号分隔，如"open,high,low,close"
+     * N 默认全部字段
+     */
+    private String fields;
+
+    /**
+     * 获取日线数据
+     * https://tushare.pro/document/2?doc_id=27
+     */
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static final class daily {
+        /**
+         * 股票代码（支持多个股票同时提取，逗号分隔）
+         * N: 默认全部股票
+         */
+        private String ts_code;
+
+        /**
+         * 交易日期（YYYYMMDD）
+         */
+        private String trade_date;
+
+        /**
+         * 开始日期(YYYYMMDD)
+         */
+        private String start_date;
+
+        /**
+         * 结束日期(YYYYMMDD)
+         */
+        private String end_date;
+
+        /**
+         * 获取日线数据
+         * @return
+         */
+        public Flux<Response> req() {
+            return newReq("daily", JSONUtil.parseObj(this)).req();
+
+        }
+
+    }
+
+    private static TushareReq newReq(String api_name, Map<String, Object> params) {
+        return TushareReq.builder()
+                .token(TOKEN)
+                .api_name(api_name)
+                .params(params)
+//                .fields("")
+                .build();
+
+    }
+
+    private Flux<Response> req() {
+        return HttpUtils.postJSON(API)
+                .body(JSONUtil.toJsonStr(this))
+                .req();
+    }
+}
