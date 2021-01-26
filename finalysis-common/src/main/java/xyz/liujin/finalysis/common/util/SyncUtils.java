64af94d717ff7ef4a0ab0838b59a77f0ca2110e6
liubaozhu <liubaozhu@hikvision.com.cn>
2021-01-26 19:01:17 +0800

    添加 SyncUtils 同步类

---
+++ finalysis-common/src/main/java/xyz/liujin/finalysis/common/util/SyncUtils.java
@@ -0,0 +1,32 @@
+package xyz.liujin.finalysis.common.util;
+
+import java.util.concurrent.Semaphore;
+
+
+public class SyncUtils {
+    private static Semaphore semaphore = new Semaphore(1);
+
+    /**
+     * 方法启动后，最少等待 {@param millis} 后才允许其他方法执行
+     * 用于控制方法执行速率
+     *
+     * @param millis 再锁上最少需要等待的事件
+     * @throws InterruptedException
+     */
+    public static void waitMillis(long millis) throws InterruptedException {
+        semaphore.acquire();
+        System.out.println("acquire 1");
+        // 再另一个线程中等待 millis 后释放锁
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(millis);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+                System.out.println("release 1");
+            }
+        };
+        new Thread(runnable).start();
+    }
+}
