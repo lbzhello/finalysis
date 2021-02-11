b0ff227cb9853ed6d20c3edf65a5d7de7a33a763
lbzhello <i191908577>
2021-02-12 00:20:57 +0800

    添加 swagger 支持

--- finalysis-analysis/src/main/java/xyz/liujin/finalysis/analysis/controller/KLineController.java
+++ finalysis-analysis/src/main/java/xyz/liujin/finalysis/analysis/controller/KLineController.java
@@ -1,5 +1,8 @@
 package xyz.liujin.finalysis.analysis.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -9,17 +12,20 @@ import xyz.liujin.finalysis.common.service.KLineService;
 /**
  * 日线数据
  */
+@Api(tags = "日线数据")
 @RestController
 @RequestMapping("/k")
 public class KLineController {
     @Autowired
     private KLineService kLineService;
 
+    @ApiOperation(value = "获取股票日线数据")
     @GetMapping
-    public Flux<KLineDto> query(@RequestParam(name = "code", required = false) String code) {
+    public Flux<KLineDto> query(@ApiParam(name = "code", value = "股票代码") @RequestParam(name = "code", required = false) String code) {
         return kLineService.getByCode(code, null, null);
     }
 
+    @ApiOperation(value = "过去股票当日 k 线")
     @GetMapping("{code}/day")
     public Flux<KLineDto> queryDay(@PathVariable(name = "code") String code) {
         return query(code);
