31b7d858c4aef5bdc87f2c63470f836e1b4fe7b3
lbzhello <i191908577>
2021-04-24 23:30:07 +0800

    sql 脚本修改，不同均线 5日 10日 20日 30日放在不同表中

--- finalysis-start/src/main/resources/sql/init.sql
+++ finalysis-start/src/main/resources/sql/init.sql
@@ -1,39 +1,40 @@
-drop table if exists stock;
-create table stock (
-    stock_code varchar(6) not null,
-    stock_name varchar(32) not null default '',
-    board smallint not null default 0,
-    stat smallint not null default 0,
-    listing_date date not null default now()
+create table if not exists stock
+(
+    stock_code   varchar(6)  not null,
+    stock_name   varchar(32) not null default '',
+    board        smallint    not null default 0,
+    stat         smallint    not null default 0,
+    listing_date date        not null default now()
 );
 
-alter table stock add constraint stock_pk primary key (stock_code);
+alter table stock
+    add constraint stock_pk primary key (stock_code);
 
 comment on table stock is '沪深股票信息';
 comment on column stock.stock_code is '股票代码';
-comment on column  stock.stock_name is '股票名称';
+comment on column stock.stock_name is '股票名称';
 comment on column stock.board is '交易板块。0 未知；1 沪 A；2 深 A；3 创业板；4 科创板';
 comment on column stock.stat is '股票状态。-1 退市；0 正常；1 融资融券；2 ST';
 
 -- 股票日 k 数据
-drop table if exists k_line;
-create table k_line (
-    id bigserial,
-	stock_code varchar(6) not null,
-	date date not null default now(),
-	open decimal(7, 2) not null default 0.00,
-	close decimal(7, 2) not null default 0.00,
-	high decimal(7, 2) not null default 0.00,
-	low decimal(7, 2) not null default 0.00,
-    change decimal(7, 2) not null default 0.00,
-    pct_change decimal(6, 2) not null default 0.00,
-	volume bigint not null default 0,
-	amount decimal(14, 2) not null default 0.00,
-	volume_ratio decimal(6, 2) not null default 0.00,
-	turn decimal(6, 2) not null default 0.00,
-	committee decimal(5, 2) not null default 0.00,
-	selling decimal(14, 2) not null default 0.00,
-	buying decimal(14, 2) not null default 0.00
+create table if not exists k_line
+(
+    id           bigserial,
+    stock_code   varchar(6)     not null,
+    date         date           not null default now(),
+    open         decimal(7, 2)  not null default 0.00,
+    close        decimal(7, 2)  not null default 0.00,
+    high         decimal(7, 2)  not null default 0.00,
+    low          decimal(7, 2)  not null default 0.00,
+    change       decimal(7, 2)  not null default 0.00,
+    pct_change   decimal(6, 2)  not null default 0.00,
+    volume       bigint         not null default 0,
+    amount       decimal(14, 2) not null default 0.00,
+    volume_ratio decimal(6, 2)  not null default 0.00,
+    turn         decimal(6, 2)  not null default 0.00,
+    committee    decimal(5, 2)  not null default 0.00,
+    selling      decimal(14, 2) not null default 0.00,
+    buying       decimal(14, 2) not null default 0.00
 ) partition by range (date);
 
 comment on table k_line is '日 K 线';
@@ -56,26 +57,30 @@ comment on column k_line.selling is '卖盘/外盘';
 
 -- 创建分区
 create table k_line_1990_2019 partition of k_line
-for values from ('1990-01-01') to ('2020-01-01');
+    for values from ('1990-01-01') to ('2020-01-01');
 
-alter table k_line_1990_2019 add constraint pk_k_line_1990_2019 primary key (id);
-create unique index uk_k_line_1990_2019_stock_code_date on k_line_1990_2019(stock_code, date);
+alter table k_line_1990_2019
+    add constraint pk_k_line_1990_2019 primary key (id);
+
+create unique index uk_k_line_1990_2019_stock_code_date on k_line_1990_2019 (stock_code, date);
 
 create table k_line_2020_2039 partition of k_line
-for values from ('2020-01-01') to ('2039-01-01');
+    for values from ('2020-01-01') to ('2039-01-01');
 
-alter table k_line_2020_2039 add constraint pk_k_line_2020_2039 primary key (id);
-create unique index uk_k_line_2020_2039_stock_code_date on k_line_2020_2039(stock_code, date);
+alter table k_line_2020_2039
+    add constraint pk_k_line_2020_2039 primary key (id);
+
+create unique index uk_k_line_2020_2039_stock_code_date on k_line_2020_2039 (stock_code, date);
 
 -- 股票日均线表
-drop table if exists avg_line;
-create table avg_line (
-    id bigserial,
-    stock_code varchar(6) not null,
-    date date not null default now(),
-    statistic integer not null default 0,
-    current decimal(7, 2) not null default 0,
-    avg decimal(7, 2) not null default 0
+create table if not exists avg_line
+(
+    id         bigserial,
+    stock_code varchar(6)    not null,
+    date       date          not null default now(),
+    statistic  integer       not null default 0,
+    current    decimal(7, 2) not null default 0,
+    avg        decimal(7, 2) not null default 0
 );
 
 comment on table avg_line is '均线数据';
@@ -86,5 +91,98 @@ comment on column avg_line.statistic is '统计天数';
 comment on column avg_line.current is '当日价格';
 comment on column avg_line.avg is 'count 均值';
 
-alter table avg_line add constraint pk_avg_line primary key (id);
+alter table avg_line
+    add constraint pk_avg_line primary key (id);
+
 create unique index uk_avg_line_stock_code_date_statistic on avg_line (stock_code, date, statistic);
+
+-- 2021-04-24
+-- 不同日期均线分割在不同表中
+
+-- 股票 5 日均线表
+create table if not exists avg_line_5
+(
+    id         bigserial,
+    stock_code varchar(6)    not null,
+    date       date          not null default now(),
+    current    decimal(7, 2) not null default 0,
+    avg        decimal(7, 2) not null default 0
+);
+
+comment on table avg_line_5 is '均线数据';
+comment on column avg_line_5.id is '自增主键';
+comment on column avg_line_5.stock_code is '股票代码。如 000001';
+comment on column avg_line_5.date is '日期';
+comment on column avg_line_5.current is '当日价格';
+comment on column avg_line_5.avg is 'count 均值';
+
+alter table avg_line_5
+    add constraint pk_avg_line_5 primary key (id);
+
+create unique index uk_avg_line_5_stock_code_date on avg_line_5 (stock_code, date);
+
+-- 股票 10 日均线表
+create table if not exists avg_line_10
+(
+    id         bigserial,
+    stock_code varchar(6)    not null,
+    date       date          not null default now(),
+    current    decimal(7, 2) not null default 0,
+    avg        decimal(7, 2) not null default 0
+);
+
+comment on table avg_line_10 is '均线数据';
+comment on column avg_line_10.id is '自增主键';
+comment on column avg_line_10.stock_code is '股票代码。如 000001';
+comment on column avg_line_10.date is '日期';
+comment on column avg_line_10.current is '当日价格';
+comment on column avg_line_10.avg is 'count 均值';
+
+alter table avg_line_10
+    add constraint pk_avg_line_10 primary key (id);
+
+create unique index uk_avg_line_10_stock_code_date on avg_line_10 (stock_code, date);
+
+-- 股票 20 日均线表
+create table if not exists avg_line_20
+(
+    id         bigserial,
+    stock_code varchar(6)    not null,
+    date       date          not null default now(),
+    current    decimal(7, 2) not null default 0,
+    avg        decimal(7, 2) not null default 0
+);
+
+comment on table avg_line_20 is '均线数据';
+comment on column avg_line_20.id is '自增主键';
+comment on column avg_line_20.stock_code is '股票代码。如 000001';
+comment on column avg_line_20.date is '日期';
+comment on column avg_line_20.current is '当日价格';
+comment on column avg_line_20.avg is 'count 均值';
+
+alter table avg_line_20
+    add constraint pk_avg_line_20 primary key (id);
+
+create unique index uk_avg_line_20_stock_code_date on avg_line_20 (stock_code, date);
+
+-- 股票 30 日均线表
+create table if not exists avg_line_30
+(
+    id         bigserial,
+    stock_code varchar(6)    not null,
+    date       date          not null default now(),
+    current    decimal(7, 2) not null default 0,
+    avg        decimal(7, 2) not null default 0
+);
+
+comment on table avg_line_30 is '均线数据';
+comment on column avg_line_30.id is '自增主键';
+comment on column avg_line_30.stock_code is '股票代码。如 000001';
+comment on column avg_line_30.date is '日期';
+comment on column avg_line_30.current is '当日价格';
+comment on column avg_line_30.avg is 'count 均值';
+
+alter table avg_line_30
+    add constraint pk_avg_line_30 primary key (id);
+
+create unique index uk_avg_line_30_stock_code_date on avg_line_30 (stock_code, date);
--- finalysis-start/src/main/resources/sql/update.sql
+++ finalysis-start/src/main/resources/sql/update.sql
@@ -1,6 +1,5 @@
 -- 2021-01-01
-drop table if exists stock;
-create table stock (
+create table if not exists stock (
     stock_code varchar(6) not null,
     stock_name varchar(32) not null default '',
     board smallint not null default 0,
@@ -16,8 +15,7 @@ comment on column stock.board is '交易板块。0 未知；1 沪 A；2 深 A；
 comment on column stock.stat is '股票状态。-1 退市；0 正常；1 融资融券；2 ST';
 
 -- 股票日 k 数据
-drop table if exists k_line;
-create table k_line (
+create table if not exists k_line (
     id bigserial,
     stock_code varchar(6) not null,
     date_time timestamp(0) with time zone not null default now(),
@@ -111,40 +109,9 @@ create unique index uk_k_line_2020_2039_stock_code_date on k_line_2020_2039(stoc
 alter table k_line attach partition k_line_2020_2039
     for values from ('2020-01-01') to ('2040-01-01');
 
--- 2021-01-31
--- 新增 avg_line 均线表
-drop table if exists avg_line;
-create table avg_line (
-    id bigserial,
-    stock_code varchar(6) not null,
-    date date not null default now(),
-    current decimal(7, 2) not null default 0,
-    avg5 decimal(7, 2) not null default 0,
-    avg10 decimal(7, 2) not null default 0,
-    avg20 decimal(7, 2) not null default 0,
-    avg30 decimal(7, 2) not null default 0
-);
-
-comment on table avg_line is '均线数据';
-comment on column avg_line.id is '自增主键';
-comment on column avg_line.stock_code is '股票代码。如 000001';
-comment on column avg_line.date is '日期';
-comment on column avg_line.current is '当日价格';
-comment on column avg_line.avg5 is '5 日均线';
-comment on column avg_line.avg10 is '10 日均线';
-comment on column avg_line.avg20 is '20 日均线';
-comment on column avg_line.avg30 is '30 日均线';
-
-alter table avg_line add constraint pk_avg_line primary key (id);
-create unique index uk_avg_line_stock_code_date on avg_line (stock_code, date);
-
 -- 2021-02-20
--- avg_line 表结构改变，删掉重建
-drop table if exists avg_line;
-
 -- 重构 avg_line 均线表
-drop table if exists avg_line;
-create table avg_line (
+create table if not exists avg_line (
     id bigserial,
     stock_code varchar(6) not null,
     date date not null default now(),
@@ -172,3 +139,110 @@ alter table avg_line rename column count to statistic;
 
 drop index uk_avg_line_stock_code_date_count;
 create unique index uk_avg_line_stock_code_date_statistic on avg_line (stock_code, date, statistic);
+
+-- 2021-04-24
+-- 均线表分开计算
+
+-- 股票 5 日均线表
+create table if not exists avg_line_5 (
+    id bigserial,
+    stock_code varchar(6) not null,
+    date date not null default now(),
+    current decimal(7, 2) not null default 0,
+    avg decimal(7, 2) not null default 0
+);
+
+comment on table avg_line_5 is '均线数据';
+comment on column avg_line_5.id is '自增主键';
+comment on column avg_line_5.stock_code is '股票代码。如 000001';
+comment on column avg_line_5.date is '日期';
+comment on column avg_line_5.current is '当日价格';
+comment on column avg_line_5.avg is 'count 均值';
+
+alter table avg_line_5 add constraint pk_avg_line_5 primary key (id);
+create unique index uk_avg_line_5_stock_code_date on avg_line_5 (stock_code, date);
+
+-- 从 avg_line 中提取 5 日均线
+insert into avg_line_5(stock_code, date, current, avg)
+select stock_code, date, current, avg from avg_line where statistic = 5;
+
+select count(*) from avg_line_5;
+
+
+-- 股票 10 日均线表
+create table if not exists avg_line_10 (
+    id bigserial,
+    stock_code varchar(6) not null,
+    date date not null default now(),
+    current decimal(7, 2) not null default 0,
+    avg decimal(7, 2) not null default 0
+);
+
+comment on table avg_line_10 is '均线数据';
+comment on column avg_line_10.id is '自增主键';
+comment on column avg_line_10.stock_code is '股票代码。如 000001';
+comment on column avg_line_10.date is '日期';
+comment on column avg_line_10.current is '当日价格';
+comment on column avg_line_10.avg is 'count 均值';
+
+alter table avg_line_10 add constraint pk_avg_line_10 primary key (id);
+create unique index uk_avg_line_10_stock_code_date on avg_line_10 (stock_code, date);
+
+-- 从 avg_line 中提取 10 日均线
+insert into avg_line_10(stock_code, date, current, avg)
+select stock_code, date, current, avg from avg_line where statistic = 10;
+
+select count(*) from avg_line_10;
+
+
+-- 股票 20 日均线表
+create table if not exists avg_line_20 (
+    id bigserial,
+    stock_code varchar(6) not null,
+    date date not null default now(),
+    current decimal(7, 2) not null default 0,
+    avg decimal(7, 2) not null default 0
+);
+
+comment on table avg_line_20 is '均线数据';
+comment on column avg_line_20.id is '自增主键';
+comment on column avg_line_20.stock_code is '股票代码。如 000001';
+comment on column avg_line_20.date is '日期';
+comment on column avg_line_20.current is '当日价格';
+comment on column avg_line_20.avg is 'count 均值';
+
+alter table avg_line_20 add constraint pk_avg_line_20 primary key (id);
+create unique index uk_avg_line_20_stock_code_date on avg_line_20 (stock_code, date);
+
+-- 从 avg_line 中提取 20 日均线
+insert into avg_line_20(stock_code, date, current, avg)
+select stock_code, date, current, avg from avg_line where statistic = 20;
+
+select count(*) from avg_line_20;
+
+
+-- 股票 30 日均线表
+create table if not exists avg_line_30 (
+    id bigserial,
+    stock_code varchar(6) not null,
+    date date not null default now(),
+    current decimal(7, 2) not null default 0,
+    avg decimal(7, 2) not null default 0
+);
+
+comment on table avg_line_30 is '均线数据';
+comment on column avg_line_30.id is '自增主键';
+comment on column avg_line_30.stock_code is '股票代码。如 000001';
+comment on column avg_line_30.date is '日期';
+comment on column avg_line_30.current is '当日价格';
+comment on column avg_line_30.avg is 'count 均值';
+
+alter table avg_line_30 add constraint pk_avg_line_30 primary key (id);
+create unique index uk_avg_line_30_stock_code_date on avg_line_30 (stock_code, date);
+
+-- 从 avg_line 中提取 30 日均线
+insert into avg_line_30(stock_code, date, current, avg)
+select stock_code, date, current, avg from avg_line where statistic = 30;
+
+select count(*) from avg_line_30;
+
